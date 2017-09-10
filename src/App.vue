20a9b1c2b40c8f8076761c578a26e0ce4a0b6e3f
Maurizio Bonani <maurizio.bonani@vecomp.it>
2017-09-10 20:34:26 +0200

    Added watchers to x,y,w,h

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
     <div style="height: 500px; width: 500px; margin: 20px; border: 1px solid red; position: relative;">
-      <vue-draggable-resizable :x="50" :y="50" :w="400" :h="400" :parent="true" @resizing="onResize" @dragging="onDrag">
+      <vue-draggable-resizable :x="x" :y="y" :w="w" :h="h" :parent="true" @resizing="onResize" @dragging="onDrag">
         <p>Component</p>
       </vue-draggable-resizable>
+    </div>
+    <div>
+      X <input type="number" v-model.number.lazy="x"> / Y <input type="number" v-model.number="y"> <br>
+      Width <input type="number" v-model.number.lazy="w"> / Height <input type="number" v-model.number="h">
     </div>
   </div>
 </template>
@@ -15,12 +19,32 @@ export default {
   name: 'app',
   components: {
     'vue-draggable-resizable': VueDraggableResizable
+  },
+  data: function () {
+    return {
+      x: 50,
+      y: 50,
+      w: 200,
+      h: 200
+    }
+  },
+  methods: {
+    onResize: function (left, top, width, height) {
+      this.x = left
+      this.y = top
+      this.w = width
+      this.h = height
+    },
+    onDrag: function (left, top) {
+      this.x = left
+      this.y = top
+    }
   }
 }
 </script>
 
 <style>
-  .active {
+  .vdr {
     border: 1px dashed black;
   }
 </style>
--- src/components/vue-draggable-resizable.vue
+++ src/components/vue-draggable-resizable.vue
@@ -353,6 +353,36 @@ export default {
         opacity: this.opacity
       }
     }
+  },
+  watch: {
+    x: function (val) {
+      if ((this.elmX + val >= this.parentX) && (val + this.elmW <= this.parentW)) {
+        this.left = (Math.round(val / this.grid[0]) * this.grid[0])
+      }
+
+      this.$emit('resizing', this.left, this.top, this.width, this.height)
+    },
+    y: function (val) {
+      if ((this.elmY + val >= this.parentY) && (val + this.elmH <= this.parentH)) {
+        this.top = (Math.round(val / this.grid[1]) * this.grid[1])
+      }
+
+      this.$emit('resizing', this.left, this.top, this.width, this.height)
+    },
+    w: function (val) {
+      if (val > 0 && this.elmX + val <= this.parentW) {
+        this.width = (Math.round(val / this.grid[0]) * this.grid[0])
+      }
+
+      this.$emit('resizing', this.left, this.top, this.width, this.height)
+    },
+    h: function (val) {
+      if (val > 0 && this.elmY + val <= this.parentH) {
+        this.height = (Math.round(val / this.grid[1]) * this.grid[1])
+      }
+
+      this.$emit('resizing', this.left, this.top, this.width, this.height)
+    }
   }
 }
 </script>
